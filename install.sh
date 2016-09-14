0d9b9efdd74176c132a44425a4179f4f5a2d18f1
Joaquin Menchaca <jmenchaca@gobalto.com>
2016-09-14 16:02:52 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 [ $(uname -s) = "Darwin" ] || { echo 'Only runs on Mac OS X!!!' ; exit 1 ; }
 # Install Homebrew
 which -s brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
 
 # Essential Brew Stuffs
 brew tap caskroom/cask
